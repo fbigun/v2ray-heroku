8eb671adf2a72bfbadb84081e7e6483b0a0193a5
fbigun <olutyo@gmail.com>
2019-08-27 11:44:56 +0800

    修改

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 sed -i "s/66666/$PORT/g" /etc/v2ray/config.json
 sed -i "s/your_uuid/$UUID/g" /etc/v2ray/config.json
 
-/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
+#/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
